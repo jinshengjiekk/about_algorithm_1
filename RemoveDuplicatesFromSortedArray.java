250978b4c6d8871c829b5cb6e07cd3b4600020d2
Hello King <jinshengjiekk@163.com>
2016-10-15 12:05:17 +0800

    complete RemoveDuplicatesFromSortedArray 001

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by JINSHENGJIE on 16/10/14 .
  * Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
@@ -8,5 +10,29 @@
  * Subscribe to see which companies asked this question
  */
 public class RemoveDuplicatesFromSortedArray {
+    /**
+     * Remove duplicates int.
+     *
+     * @param sortedArray the sorted arrays
+     * @return the int
+     */
+    public int removeDuplicates(int[] sortedArray) {
+        int length = sortedArray.length;
+        int i = 0;
+        int j = 1;
+        for (; i < length ; i++, j++) {
+            if (sortedArray[--j] == sortedArray[i]) {
+                continue;
+            }
+            sortedArray[++j] = sortedArray[i];
+        }
+        System.out.println(Arrays.toString(sortedArray));
+        return j;
+    }
 
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 2, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 7, 8, 9};
+        RemoveDuplicatesFromSortedArray rdfs = new RemoveDuplicatesFromSortedArray();
+        System.out.println(rdfs.removeDuplicates(sortedArray));
+    }
 }
